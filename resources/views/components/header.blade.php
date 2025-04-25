afb5ab00e35d0a4487519c54c7cc790f3b5effe2
Ariefhuda434 <ariefhuda434@gmail.com>
2025-04-25 13:47:32 +0700

    Initial commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,4 @@
+
+<header>
+    <h1>ini halaman {{ $slot }}</h1>
+</header>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="bg-olive p-5 flex justify-between items-center" >
+    <div class="font-bold text-xl">Logo</div>
+    <ul class="flex space-x-6 justift-center items-center flex 1 font-roboto text-xl">
+        <li class=" text-white hover:text-olivedarker-700 transition duration-700 ease-in-out"><a href="">Beranda</a></li>
+        <li class=" text-white hover:text-green-700 transition duration-700 ease-in-out"><a href="">Tentang</a></li>
+        <li class=" text-white hover:text-green-700 transition duration-700 ease-in-out"><a href="">Blog & Media</a></li>
+        <li class=" text-white hover:text-green-700 transition duration-700 ease-in-out"><a href="">Event</a></li>
+        <li class=" text-white hover:text-green-700 transition duration-700 ease-in-out"><a href="">FAQ</a></li>
+    </ul>
+    <div class="w-24 font-bold text-xl">Masyuk</div>
+</nav>
