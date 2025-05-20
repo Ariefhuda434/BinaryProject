613a1554a35572087158cd870ccd4443fb9ad7fb
nahyasyakiraa <nahyasyakiraketaren@gmail.com>
2025-05-20 10:50:56 +0700

    nahya ngetes

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <x-header>faq</x-header>
     <details class="border-b mt-20 py-4">
-  <summary class="font-semibold text-lg cursor-pointer text-gray-800">Bagaimana cara membuat laporan sampah?</summary>
+  <summary class="font-semibold text-lg cursor-pointer text-gray-800">Bagaimana cara membuat</summary>
   <p class="text-gray-600 mt-2 text-sm">Anda bisa login ke akun, lalu pilih menu 'Lapor Sampah' dan isi formulir yang tersedia.</p>
 </details>
 
